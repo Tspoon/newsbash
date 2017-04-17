e7eef467fc68555a75f4aaf52bb782fdd35a62d6
Thomas Stenhouse-Pyne <tommpyne@gmail.com>
2017-04-17 15:36:43 +0100

    Fix malformed HTML from newssource breaking script.
    Tidy up xmllint calls by extracting xpaths.
    Rename title to headlines, for consistency.

--- newsbash.sh
+++ newsbash.sh
@@ -19,10 +19,10 @@ set -o pipefail
 headlinesOnly=false
 
 # Parse arguments
-while [[ $# -gt 0 ]]
+while [[ "$#" -gt 0 ]]
 do
 	arg="$1"
-	case $arg in
+	case "$arg" in
 		-s|--short)
 			headlinesOnly=true
 			;;
@@ -34,38 +34,43 @@ done
 RSS=$(curl -L --silent http://www.theweek.co.uk/feeds/all)
 
 # Parse the latest 'Ten Things' article from the RSS feed.
-HTML=$(echo $RSS | xmllint --xpath "string(//item[contains(title, 'Ten Things You Need to Know Today')]/description)" -)
+rssXpath="string(//item[contains(title, 'Ten Things You Need to Know Today')]/description)"
+HTML=$(echo "$RSS" | xmllint --xpath "$rssXpath" -)
 
 # Count the number of items in the 'Ten Things' article. This isn't strictly necessary, but good to check.
-numItems=$(echo $HTML | xmllint --xpath "count(//div[contains(@class, 'story-headline')]/div/div)" -) 
+countXpath="count(//div[contains(@class, 'story-headline')]/div/div)"
+numItems=$(echo "$HTML" | xmllint --recover --xpath "$countXpath" - 2>/dev/null)
 
-for ((i=1; i<=$numItems; i++))
+for ((i=1; i<="$numItems"; i++))
 do
-	# Parse the title and body from the Ten Things article
-	title=$(echo $HTML | xmllint --xpath "normalize-space(string((//div[contains(@class, 'story-headline')])[$i]))" -)
-	body=$(echo $HTML | xmllint --xpath "normalize-space(string((//div[contains(@class, 'story-body')])[$i]//p))" -)
+	headlineXpath="normalize-space(string((//div[contains(@class, 'story-headline')])[$i]))"
+	storyXpath="normalize-space(string((//div[contains(@class, 'story-body')])[$i]))"
+
+	# Parse the headline and body from the Ten Things article
+	headline=$(echo "$HTML" | xmllint --recover --xpath "$headlineXpath" - 2>/dev/null)
+	body=$(echo "$HTML" | xmllint --recover --xpath "$storyXpath" - 2>/dev/null)
 
 	# Get the terminal width and define the output string.
 	termWidth=$(tput cols)
 
-	if [[ $headlinesOnly = true ]]
+	if [[ "$headlinesOnly" = true ]]
 	then 
-		output="$title"
+		output="$headline"
 	else
-		output="\e[1m$title\n\e[0m$body"
+		output="\e[1m$headline\n\e[0m$body"
 
-		if [[ $i -lt $numItems ]]
+		if [[ "$i" -lt "$numItems" ]]
 		then
 			output="${output}\n\n"
 		fi
 	fi
 
 	# Print the headline and body at the appropriate width.
-	if [[ $termWidth -lt $maxOutputWidth ]]
+	if [[ "$termWidth" -lt "$maxOutputWidth" ]]
 	then
-		echo -e "$output"| fold -w $termWidth -s
+		echo -e "$output"| fold -w "$termWidth" -s
 	else
-		echo -e "$output" | fold -w $maxOutputWidth -s
+		echo -e "$output" | fold -w "$maxOutputWidth" -s
 	fi
 
 done
